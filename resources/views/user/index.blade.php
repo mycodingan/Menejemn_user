2fada662cd001ec3c1f2392c7ef9139a265ee621
joshua <joshuapplg@gmail.com>
2024-01-22 09:25:43 +0700

    "🚀wed pled bro"

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -74,16 +74,16 @@
 
 
     <div class="btn-group mb-3">
-        <button type="button" class="btn btn-primary  mr-4 bg-success-400" onclick="switchView('card')">Card </button>
-        <button type="button" class="btn btn-primary  mr-4 bg-success-400" onclick="switchView('table')">Table</button>
-        <button class="btn  mr-4 bg-success-400" onclick="cariAku('cari')">serch</button>
-        <button type="button" class="btn btn-primary bg-success-400" data-toggle="modal" data-target="#exampleModalCenter">
+        <button type="button" class="btn btn-primary  mr-4 bg-dark" onclick="switchView('card')">Card </button>
+        <button type="button" class="btn btn-primary  mr-4 bg-dark" onclick="switchView('table')">Table</button>
+        <button class="btn  mr-4 bg-dark" onclick="cariAku('cari')">serch</button>
+        <button type="button" class="btn btn-primary bg-dark" data-toggle="modal" data-target="#exampleModalCenter">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6" />
             </svg>
         </button>
         <button class=" text-decoration-none" type="button">
-            <a href="{{ route('user.export') }}" class="btn  bg-success-400" style="text-decoration: "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
+            <a href="{{ route('user.export') }}" class="btn  bg-dark" style="text-decoration: "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5m-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5" />
                 </svg> </a>
         </button>
